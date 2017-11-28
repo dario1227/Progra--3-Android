4f711bda45621a591d1d398cf9ab834fa309fda6
Róger Valderrama <roger.andres18@gmail.com>
2017-11-28 00:53:26 -0600

    [NEW] UI FRAGMENTS :D

---
+++ app/src/main/java/datos1/tec/org/packettec/activities/MainActivity.java
@@ -0,0 +1,71 @@
+package datos1.tec.org.packettec.activities;
+
+
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
+
+import datos1.tec.org.packettec.R;
+import datos1.tec.org.packettec.fragments.ChatFragment;
+import datos1.tec.org.packettec.fragments.LoginFragment;
+import datos1.tec.org.packettec.fragments.MainFragment;
+import datos1.tec.org.packettec.fragments.NewMessageFragment;
+import datos1.tec.org.packettec.fragments.SearchFragment;
+
+public class MainActivity extends AppCompatActivity implements LoginFragment.LoginFragmentInteractionListener, MainFragment.OnFragmentInteractionListener, NewMessageFragment.OnFragmentInteractionListener, SearchFragment.OnFragmentInteractionListener, ChatFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new LoginFragment();
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.add(R.id.fragment_container, fragment);
+            transaction.commit();
+        }
+
+    }
+
+    public void loadMainFragment() {
+
+        MainFragment mainFragment = new MainFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, mainFragment).addToBackStack(null).commit();
+
+    }
+
+    public void loadSearchFragment() {
+
+        SearchFragment SearchFragment = new SearchFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, SearchFragment).addToBackStack(null).commit();
+
+    }
+
+    public void loadNewMessageFragment() {
+
+        NewMessageFragment newMessageFragment = new NewMessageFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, newMessageFragment).addToBackStack(null).commit();
+
+    }
+
+    public void loadChatFragment() {
+
+        ChatFragment chatFragment = new ChatFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, chatFragment).addToBackStack(null).commit();
+
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
+
