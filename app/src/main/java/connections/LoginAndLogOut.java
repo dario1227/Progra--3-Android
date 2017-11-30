236a5034446cd2d29fcdf8afcf4b084c6c913e25
Róger Valderrama <roger.andres18@gmail.com>
2017-11-30 16:15:00 -0600

    Test

--- app/src/main/java/connections/LoginAndLogOut.java
+++ app/src/main/java/connections/LoginAndLogOut.java
@@ -13,46 +13,46 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 public class LoginAndLogOut {
-    public static void LogIn(String name) {
-        try {
-            String query = "http://localhost:9080/webapi/services/auth" + "?Name=" + name;
-            URL url = new URL(query);
-
-            HttpURLConnection client = (HttpURLConnection) url.openConnection();
-            client.setRequestMethod("POST");
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-            System.out.println(response.toString());
-            client.disconnect();
-        } catch (IOException exception) {
-        }
-    }
-    public static void LogOut() {
-        try {
-            String query = "http://localhost:9080/webapi/services/auth/logOut";
-            URL url = new URL(query);
-
-            HttpURLConnection client = (HttpURLConnection) url.openConnection();
-            client.setRequestMethod("POST");
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-            System.out.println(response.toString());
-            client.disconnect();
-        } catch (IOException exception) {
-        }
-    }
+//    public static void LogIn(String name) {
+//        try {
+//            String query = "http://localhost:9080/webapi/services/auth" + "?Name=" + name;
+//            URL url = new URL(query);
+//
+//            HttpURLConnection client = (HttpURLConnection) url.openConnection();
+//            client.setRequestMethod("POST");
+//
+//            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+//            String inputLine;
+//            StringBuffer response = new StringBuffer();
+//
+//            while ((inputLine = in.readLine()) != null) {
+//                response.append(inputLine);
+//            }
+//            in.close();
+//            System.out.println(response.toString());
+//            client.disconnect();
+//        } catch (IOException exception) {
+//        }
+//    }
+//    public static void LogOut() {
+//        try {
+//            String query = "http://localhost:9080/webapi/services/auth/logOut";
+//            URL url = new URL(query);
+//
+//            HttpURLConnection client = (HttpURLConnection) url.openConnection();
+//            client.setRequestMethod("POST");
+//
+//            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+//            String inputLine;
+//            StringBuffer response = new StringBuffer();
+//
+//            while ((inputLine = in.readLine()) != null) {
+//                response.append(inputLine);
+//            }
+//            in.close();
+//            System.out.println(response.toString());
+//            client.disconnect();
+//        } catch (IOException exception) {
+//        }
+//    }
 }
--- app/src/main/java/connections/Polling.java
+++ app/src/main/java/connections/Polling.java
@@ -11,22 +11,22 @@ import datos1.tec.org.packettec.R;
  * Created by Marco Herrera on 28/11/2017.
  */
 
-public class Polling implements Runnable {
-
-    @Override
-    public void run() {
-        while (true) {
-            try {
-                JSONArray messages = new JSONArray(HttpRequest.get(R.string.url + "/messages"));
-
-                for (int index = 0; index < messages.length(); index++) {
-                    JSONObject message = messages.getJSONObject(index);
-                    //if ((JSONArray)message.get("path")...
-
-                }
-            } catch (Exception e) {
-
-            }
-        }
-    }
-}
+//public class Polling implements Runnable {
+//
+////    @Override
+////    public void run() {
+////        while (true) {
+////            try {
+////                JSONArray messages = new JSONArray(HttpRequest.get(R.string.url + "/messages"));
+////
+////                for (int index = 0; index < messages.length(); index++) {
+////                    JSONObject message = messages.getJSONObject(index);
+////                    //if ((JSONArray)message.get("path")...
+////
+////                }
+////            } catch (Exception e) {
+////
+////            }
+////        }
+////    }
+//}
