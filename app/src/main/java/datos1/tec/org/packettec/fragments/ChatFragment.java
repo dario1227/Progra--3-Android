25ca6c215d1548effc5a8ba50920b06938ac3824
m-herrera <m.herrera0799@gmail.com>
2017-12-01 00:19:12 -0600

    [Updated] Message

--- app/src/main/java/datos1/tec/org/packettec/fragments/ChatFragment.java
+++ app/src/main/java/datos1/tec/org/packettec/fragments/ChatFragment.java
@@ -79,6 +79,7 @@ public class ChatFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
 
+
         final MainActivity mainActivity = (MainActivity) getActivity();
         messageText = mainActivity.findViewById(R.id.MessageText);
         messageText.setOnKeyListener(new View.OnKeyListener() {
@@ -91,7 +92,7 @@ public class ChatFragment extends Fragment {
                         message.put("body", messageText.getText().toString());
                         message.put("sender", "Jasson");//MainActivity.myUserName);
                         HttpRequest request = new HttpRequest();
-                        //request.post(getString(R.string.url) + "messages",message.toString());
+                        request.post(getString(R.string.url) + "messages", message.toString());
                         System.out.println(message.toString());
                     }catch(Exception e){
                         e.printStackTrace();
--- app/src/main/java/datos1/tec/org/packettec/fragments/WriteFragment.java
+++ app/src/main/java/datos1/tec/org/packettec/fragments/WriteFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import datos1.tec.org.packettec.R;
 
@@ -23,6 +24,7 @@ public class WriteFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private ImageButton button;
 
 
     public WriteFragment() {
@@ -60,7 +62,18 @@ public class WriteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_write, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_write, container, false);
+
+        button = v.findViewById(R.id.attachBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return v;
     }
 
 }
